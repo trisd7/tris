25067ef57a44c3ceed2cceb782808fa824cd312f
trisd7 <tdasofficial04@gmail.com>
2025-06-21 12:19:15 +0530

    Create sjf(without).c

---
+++ sjf(without).c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+    int p[20], bt[20], wt[20], tat[20], i, k, n, temp;
+    float wtavg, tatavg;
+
+    printf("\nEnter the number of processes -- ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        p[i] = i;
+        printf("Enter Burst Time for Process %d -- ", i);
+        scanf("%d", &bt[i]);
+    }
+
+    // Sort by Burst Time (SJF)
+    for(i = 0; i < n; i++)
+    {
+        for(k = i + 1; k < n; k++)
+        {
+            if(bt[i] > bt[k])
+            {
+                temp = bt[i]; bt[i] = bt[k]; bt[k] = temp;
+                temp = p[i];  p[i] = p[k];  p[k] = temp;
+            }
+        }
+    }
+
+    wt[0] = 0;
+    tat[0] = bt[0];
+    wtavg = 0;
+    tatavg = tat[0];
+
+    for(i = 1; i < n; i++)
+    {
+        wt[i] = wt[i - 1] + bt[i - 1];
+        tat[i] = wt[i] + bt[i];  // ✅ Correct TAT
+        wtavg += wt[i];
+        tatavg += tat[i];
+    }
+
+    printf("\nPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("P%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time -- %f", wtavg / n);
+    printf("\nAverage Turnaround Time -- %f\n", tatavg / n);
+
+    return 0;
+}
